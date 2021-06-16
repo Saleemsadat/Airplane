fd43b171178235db2c1627a14abffb7955cb2568
Saleem Nasim <SNasim@acdivoca.org>
2021-06-16 11:13:57 -0400

    Airplane project assigned to Tester A and B

---
+++ src/AirplaneStructure/Instructions.java
@@ -0,0 +1,7 @@
+package AirplaneStructure;
+
+public class Instructions {
+    public static void main(String[] args) {
+        System.out.println("TesterA should work on the make TesterB Should work on the model");
+    }
+}
